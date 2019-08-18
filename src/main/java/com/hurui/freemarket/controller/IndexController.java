8b213561a982fe7d95634cbda18a1c0fd13b6202
hurui007 <hurui_h@qq.com>
2019-08-18 22:01:17 +0800

    添加新的controller‘

--- src/main/java/com/hurui/freemarket/controller/IndexController.java
+++ src/main/java/com/hurui/freemarket/controller/IndexController.java
@@ -2,15 +2,23 @@ package com.hurui.freemarket.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String welcome(Map<String, Object> model, HttpServletRequest request){
         return "index";
+    }
+
+    @RequestMapping("/cpone")
+    public String cpOne(HttpServletRequest request, HttpServletResponse response){
+        request.setAttribute("intVar", "100");
+        return "cpone";
     }
 }
