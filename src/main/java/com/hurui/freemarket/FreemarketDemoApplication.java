f5bfeec0293006ff70142b8707288c8de7b4b2c3
hurui007 <hurui_h@qq.com>
2019-08-18 21:15:36 +0800

    初始化项目

---
+++ src/main/java/com/hurui/freemarket/FreemarketDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hurui.freemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreemarketDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreemarketDemoApplication.class, args);
+	}
+
+}
